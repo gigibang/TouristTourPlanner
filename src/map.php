73538bf7644b391dc7923c5c0891fe7ec01d2b23
Dylan Nelson <dylanmnelson@yahoo.com.au>
2015-08-10 11:03:55 +1000

    Added map page

---
+++ src/map.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Planner</title>
+        <meta name="description" content="">
+		<link rel="stylesheet" href="css/normalize.css">
+		<!--Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+	</head>
+	<body>
+		<!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+		<!--START body html content-->
+		
+		<!--END body html content-->
+		<!--jQuery with offline backup if needed-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+		<script src="js/vendor/bootstrap.min.js"></script>
+	</body>
+</html>
